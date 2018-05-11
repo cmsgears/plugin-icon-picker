d8fa6c7893159a5b5eec7d47576621bf97633b10
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2018-05-11 07:32:38 +0530

    Applied quick fixes.

--- widgets/views/icon.php
+++ widgets/views/icon.php
@@ -1,15 +1,5 @@
-<?php
-/**
- * This file is part of CMSGears Framework. Please view License file distributed
- * with the source code for license details.
- *
- * @link https://www.cmsgears.org/
- * @copyright Copyright (c) 2015 VulpineCode Technologies Pvt. Ltd.
- */
-?>
-
 <?php if( isset( $label ) ) { ?>
-	<label>Icon</label>
+	<label><?= $label ?></label>
 <?php } ?>
 
 <div class="icon-picker">
